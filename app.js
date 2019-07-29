bffc7a636bd210a9d5c83cb12022c8e006b8e949
davieu <davisumana@gmail.com>
2019-07-29 02:25:24 -0400

    user saved to mongoDB and validation for new user is working also bcrypt and creating a salt to encrypt and create a hash password is working

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 // DB config
 const keys = require('./config/keys');
 
 const app = express();
+app.use(cors());
 
 // Connect to Mongo
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
@@ -17,6 +21,20 @@ const PORT = process.env.PORT || 5000;
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session middleware
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Connect flash
+app.use(flash());
 
 // Routes
 app.use('/', require('./routes/index'));
