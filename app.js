ee955f2b23a4c5d39c09c7aeb5eaba2b770a2b53
davieu <davisumana@gmail.com>
2019-07-28 23:35:59 -0400

    mongoose implented with a schema model for User also added ejs pages

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// DB config
+const keys = require('./config/keys');
+
 const app = express();
 
+// Connect to Mongo
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
 const PORT = process.env.PORT || 5000;
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
 // Routes
 app.use('/', require('./routes/index'));
---
+++ config/dev.js
@@ -0,0 +1,4 @@
+// people is the DB name in atlas
+module.exports = {
+  mongoURI: 'mongodb+srv://davieu101:davieu101@cluster0-vgyon.mongodb.net/node-passport-login?retryWrites=true&w=majority'
+}
